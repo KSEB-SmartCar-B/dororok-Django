18f7888b5943bba30fef601f2867c76dc9bea052
Hyeongseok <a1932a@naver.com>
2024-08-15 11:44:22 +0900

    requirement

--- dororok-django/crawling/views.py
+++ dororok-django/crawling/views.py
@@ -10,15 +10,13 @@ from datetime import datetime
 
 def chart_view(request, genre):
     today = datetime.now().date()
-
-    perform_update_and_analysis(genre, today)
-    # if today.weekday() == 3:
-    #     try:
-    #         last_update = LastUpdate.objects.get(genre=genre)
-    #         if last_update.last_updated.date() < today:
-    #             perform_update_and_analysis(genre, today)
-    #     except LastUpdate.DoesNotExist:
-    #         perform_update_and_analysis(genre, today)
+    if today.weekday() == 4:
+         try:
+             last_update = LastUpdate.objects.get(genre=genre)
+             if last_update.last_updated.date() < today:
+                 perform_update_and_analysis(genre, today)
+         except LastUpdate.DoesNotExist:
+             perform_update_and_analysis(genre, today)
 
     if genre not in genre_code_map:
         return render(request, 'crawling/error.html', {'message': 'Invalid genre'})
@@ -47,9 +45,9 @@ def perform_update_and_analysis(genre, today):
     #print('clear update')
 
     #search_all_genres()
-    print('clear search')
+    #print('clear search')
 
-    #analyze_data.analyze_data()
+    analyze_data.analyze_data()
     print('end')
 
     train_and_save_model()
